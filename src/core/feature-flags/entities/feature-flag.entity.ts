7dd0715b36d943699e2a09a69a1db89e16f80766
Ajao Yussuf <ajaoyussufayo@gmail.com>
2026-01-01 21:41:47 +0100

    disabled prettier

---
+++ src/core/feature-flags/entities/feature-flag.entity.ts
@@ -0,0 +1,33 @@
+import { Workspace } from 'src/modules/workspaces/entities/workspace.entity';
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  ManyToOne,
+  JoinColumn,
+  Unique,
+} from 'typeorm';
+
+
+@Entity({ name: 'feature_flags', schema: 'public' })
+@Unique(['workspaceId', 'featureKey'])
+export class FeatureFlag {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ type: 'uuid', name: 'workspace_id' })
+  workspaceId: string;
+
+  @ManyToOne(() => Workspace)
+  @JoinColumn({ name: 'workspace_id' })
+  workspace: Workspace;
+
+  @Column({ type: 'varchar', length: 100, name: 'feature_key' })
+  featureKey: string;
+
+  @Column({ type: 'boolean', default: false, name: 'is_enabled' })
+  isEnabled: boolean;
+
+  @Column({ type: 'jsonb', nullable: true })
+  metadata: Record<string, any>;
+}
