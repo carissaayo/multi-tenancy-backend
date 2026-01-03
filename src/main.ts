234c41d30eff3f1a1db1b0bb4c48a28503250a75
Ajao Yussuf <ajaoyussufayo@gmail.com>
2026-01-03 21:36:21 +0100

    installed swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
     app.setGlobalPrefix('api');
+     const config = new DocumentBuilder()
+       .setTitle('Multi-Tenancy API')
+       .setDescription('API documentation for the Multi-Tenancy backend')
+       .setVersion('1.0')
+       .addBearerAuth(
+         {
+           type: 'http',
+           scheme: 'bearer',
+           bearerFormat: 'JWT',
+         },
+         'access-token',
+       )
+       .build();
+
+     const document = SwaggerModule.createDocument(app, config);
+     SwaggerModule.setup('api/docs', app, document);
   const port = process.env.PORT || 8000;
   await app.listen(port);
 
