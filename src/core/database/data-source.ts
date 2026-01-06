6be3cccb076329d7f25623f326a2b3c0c2e66453
Ajao Yussuf <ajaoyussufayo@gmail.com>
2026-01-06 13:26:04 +0100

    fixing migrations

--- src/core/database/data-source.ts
+++ src/core/database/data-source.ts
@@ -1,14 +1,27 @@
+// src/core/database/data-source.ts
 import { DataSource } from 'typeorm';
 import { join } from 'path';
 import * as dotenv from 'dotenv';
 
-//  Load env FIRST (TypeORM CLI does NOT do this for you)
+// Load env FIRST (TypeORM CLI does NOT do this for you)
 dotenv.config({ path: join(__dirname, '../../../.env') });
 
 import config from '../../config/config';
 
-
 const appConfig = config();
+
+// Import all entities explicitly
+import { User } from '../../modules/users/entities/user.entity';
+import { Workspace } from '../../modules/workspaces/entities/workspace.entity';
+import { FeatureFlag } from '../feature-flags/entities/feature-flag.entity';
+import { UsageMetric } from '../usage/entities/usage.entity';
+import { RefreshToken } from '../security/entities/refresh-token.entity';
+import { WorkspaceMemberEntity } from '../../modules/members/entities/member.entity';
+import { ChannelEntity } from '../../modules/channels/entities/channel.entity';
+import { ChannelMemberEntity } from '../../modules/channels/entities/channel-member.entity';
+import { MessageEntity } from '../../modules/messages/entities/message.entity';
+import { FileEntity } from '../../modules/files/entities/file.entity';
+import { ReactionEntity } from '../../modules/reactions/entities/reaction.entity';
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
@@ -18,7 +31,24 @@ export const AppDataSource = new DataSource({
   password: appConfig.database.password,
   database: appConfig.database.database,
 
-  entities: [join(__dirname, '../../**/*.entity{.ts,.js}')],
+  // Explicitly list all entities (works better in Docker)
+  entities: [
+    // Public schema entities
+    User,
+    Workspace,
+    FeatureFlag,
+    UsageMetric,
+    RefreshToken,
+
+    // Tenant schema entities (EntitySchema)
+    WorkspaceMemberEntity,
+    ChannelEntity,
+    ChannelMemberEntity,
+    MessageEntity,
+    FileEntity,
+    ReactionEntity,
+  ],
+
   migrations: [join(__dirname, '../../database/migrations/*{.ts,.js}')],
 
   synchronize: false,
