24be543be0d635b06c05ed6a7f8886c0a3e61715
Ajao Yussuf <ajaoyussufayo@gmail.com>
2026-01-11 09:03:14 +0100

    added aws to user module

--- src/modules/users/user.module.ts
+++ src/modules/users/user.module.ts
@@ -1,16 +1,18 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './entities/user.entity';
-import { UsersService } from './services/user.service';
+
 import { SecurityModule } from 'src/core/security/security.module';
+import { UsersService } from './services/user.service';
+import { AWSStorageService } from 'src/core/storage/services/aws-storage.service';
+
 import { UsersController } from './controllers/user.controller';
 
+import { User } from './entities/user.entity';
+
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([User]), SecurityModule
-],
-  providers: [UsersService],
-  controllers:[UsersController],
+  imports: [TypeOrmModule.forFeature([User]), SecurityModule],
+  providers: [UsersService, AWSStorageService],
+  controllers: [UsersController],
   exports: [UsersService],
 })
 export class UserModule {}
