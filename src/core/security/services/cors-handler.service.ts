6d75031f46c218148414b1591355f8af73c07a82
Ajao Yussuf <ajaoyussufayo@gmail.com>
2026-01-27 11:28:38 +0100

    adding refresh token to the cors headers

--- src/core/security/services/cors-handler.service.ts
+++ src/core/security/services/cors-handler.service.ts
@@ -22,7 +22,7 @@ export class CorsHandler {
         );
         res.setHeader(
           'Access-Control-Allow-Headers',
-          'Content-Type, Authorization, X-Requested-With, X-API-Key, x-workspace-slug',
+          'Content-Type, Authorization, X-Requested-With, X-API-Key, x-workspace-slug, x-refresh-token',
         );
         res.setHeader('Access-Control-Allow-Credentials', 'true');
         res.setHeader('Access-Control-Max-Age', '86400');
