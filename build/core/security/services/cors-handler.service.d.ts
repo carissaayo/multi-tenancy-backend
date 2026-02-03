0857921cb52f370c8535360415c50929a7a15bec
Ajao Yussuf <ajaoyussufayo@gmail.com>
2026-02-03 17:14:44 +0100

    new build

--- build/core/security/services/cors-handler.service.d.ts
+++ build/core/security/services/cors-handler.service.d.ts
@@ -1,5 +1,7 @@
 import type { Request, Response } from 'express';
 export declare class CorsHandler {
     private readonly allowedOrigins;
+    private readonly logger;
+    constructor();
     handleCORS(req: Request, res: Response): boolean;
 }
--- build/core/security/services/cors-handler.service.js
+++ build/core/security/services/cors-handler.service.js
@@ -5,13 +5,36 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+var CorsHandler_1;
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.CorsHandler = void 0;
 const common_1 = require("@nestjs/common");
-let CorsHandler = class CorsHandler {
-    allowedOrigins = process.env.ALLOWED_ORIGINS?.split(',') || [
-        'http://localhost:3000',
-    ];
+let CorsHandler = CorsHandler_1 = class CorsHandler {
+    allowedOrigins = (() => {
+        const origins = [];
+        origins.push('http://localhost:3000');
+        if (process.env.FRONTEND_URL) {
+            const frontendUrl = process.env.FRONTEND_URL.trim().replace(/\/$/, '');
+            if (frontendUrl) {
+                origins.push(frontendUrl);
+            }
+        }
+        if (process.env.ALLOWED_ORIGINS) {
+            const additionalOrigins = process.env.ALLOWED_ORIGINS
+                .split(',')
+                .map(origin => origin.trim().replace(/\/$/, ''))
+                .filter(origin => origin.length > 0);
+            origins.push(...additionalOrigins);
+        }
+        return [...new Set(origins)];
+    })();
+    logger = new common_1.Logger(CorsHandler_1.name);
+    constructor() {
+        this.logger.log(`CORS enabled for origins: ${JSON.stringify(this.allowedOrigins)}`);
+    }
     handleCORS(req, res) {
         const origin = req.headers.origin;
         const method = req.method;
@@ -24,6 +47,9 @@ let CorsHandler = class CorsHandler {
                 res.setHeader('Access-Control-Allow-Credentials', 'true');
                 res.setHeader('Access-Control-Max-Age', '86400');
             }
+            else {
+                this.logger.warn(`CORS blocked OPTIONS request from origin: ${origin}. Allowed origins: ${JSON.stringify(this.allowedOrigins)}`);
+            }
             res.status(204).end();
             return false;
         }
@@ -31,11 +57,15 @@ let CorsHandler = class CorsHandler {
             res.setHeader('Access-Control-Allow-Origin', origin);
             res.setHeader('Access-Control-Allow-Credentials', 'true');
         }
+        else if (origin) {
+            this.logger.warn(`CORS blocked ${method} request from origin: ${origin}. Allowed origins: ${JSON.stringify(this.allowedOrigins)}`);
+        }
         return true;
     }
 };
 exports.CorsHandler = CorsHandler;
-exports.CorsHandler = CorsHandler = __decorate([
-    (0, common_1.Injectable)()
+exports.CorsHandler = CorsHandler = CorsHandler_1 = __decorate([
+    (0, common_1.Injectable)(),
+    __metadata("design:paramtypes", [])
 ], CorsHandler);
 //# sourceMappingURL=cors-handler.service.js.map
